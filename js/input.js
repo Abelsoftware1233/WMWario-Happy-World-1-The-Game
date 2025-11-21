d94ab2b68ffeae3f91e70a3626f060c3f51b79b7
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-21 23:27:25 +0100

    Update input.js

--- js/input.js
+++ js/input.js
@@ -2,6 +2,7 @@
     var pressedKeys = {};
 
     function setKey(event, status) {
+        // Bestaande toetsenbord logica
         var code = event.keyCode;
         var key;
 
@@ -16,9 +17,9 @@
             key = 'RIGHT'; break;
         case 40:
             key = 'DOWN'; break;
-        case 88:
+        case 88: // X
             key = 'JUMP'; break;
-        case 90:
+        case 90: // Z
             key = 'RUN'; break;
         default:
             key = String.fromCharCode(code);
@@ -27,6 +28,7 @@
         pressedKeys[key] = status;
     }
 
+    // --- Toetsenbord Event Listeners ---
     document.addEventListener('keydown', function(e) {
         setKey(e, true);
     });
@@ -35,7 +37,63 @@
         setKey(e, false);
     });
 
+    // --- Touchscreen Logica: Nieuw Toegevoegd! ---
+    
+    function setupTouchControls() {
+        var touchControls = {
+            'touch-left': 'LEFT',
+            'touch-right': 'RIGHT',
+            'touch-jump': 'JUMP',
+            'touch-run': 'RUN'
+        };
+
+        // Koppel elke knop aan de juiste game-actie
+        for (var id in touchControls) {
+            var element = document.getElementById(id);
+            if (element) {
+                var action = touchControls[id];
+                
+                // Bij aanraking (touchstart): Zet de toets op 'ingedrukt'
+                element.addEventListener('touchstart', function(e) {
+                    e.preventDefault(); // Voorkomt standaard browser acties zoals scrollen/zoomen
+                    var keyToPress = touchControls[this.id];
+                    pressedKeys[keyToPress] = true;
+                });
+
+                // Bij loslaten (touchend/touchcancel): Zet de toets op 'niet ingedrukt'
+                element.addEventListener('touchend', function(e) {
+                    e.preventDefault();
+                    var keyToRelease = touchControls[this.id];
+                    pressedKeys[keyToRelease] = false;
+                });
+                
+                // 'touchcancel' voor als de vinger van het element afglijdt
+                element.addEventListener('touchcancel', function(e) {
+                    e.preventDefault();
+                    var keyToRelease = touchControls[this.id];
+                    pressedKeys[keyToRelease] = false;
+                });
+                
+                // Voor desktop debugging, kun je ook mousedown/up toevoegen:
+                element.addEventListener('mousedown', function() {
+                    var keyToPress = touchControls[this.id];
+                    pressedKeys[keyToPress] = true;
+                });
+                element.addEventListener('mouseup', function() {
+                    var keyToRelease = touchControls[this.id];
+                    pressedKeys[keyToRelease] = false;
+                });
+            }
+        }
+    }
+    
+    // Voer de setup pas uit nadat de pagina volledig is geladen
+    window.addEventListener('load', setupTouchControls);
+    
+    // --- Algemene Event Listeners (Bestaand) ---
+
     window.addEventListener('blur', function() {
+        // Reset alle ingedrukte toetsen wanneer het venster de focus verliest
         pressedKeys = {};
     });
 
@@ -44,6 +102,7 @@
             return pressedKeys[key.toUpperCase()];
         },
         reset: function() {
+          // Resetten voor game-logica (bestaand)
           pressedKeys['RUN'] = false;
           pressedKeys['LEFT'] = false;
           pressedKeys['RIGHT'] = false;
